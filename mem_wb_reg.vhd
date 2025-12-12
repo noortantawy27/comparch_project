3b0d86157d23ef3b58e68c71d4a3d617e54747dd
Omar Reda <omar.reda.eng@gmail.com>
2025-12-12 02:11:12 +0200

    Finished write back but simulation fails so can't test yet. Commented out memwrite control signal in MEM/WB pipeline register file

--- mem_wb_reg.vhd
+++ mem_wb_reg.vhd
@@ -4,31 +4,31 @@ entity mem_wb_reg is
     port(
         clk,rst, enable:in std_logic; 
         memory_d: in std_logic_vector(31 downto 0);
-        memory_q: out std_logic_vector(31 downto 0);
+        memory_q: out std_logic_vector(31 downto 0); ---------
         alu_d:in std_logic_vector(31 downto 0);
-        alu_q:out std_logic_vector(31 downto 0);
+        alu_q:out std_logic_vector(31 downto 0); ----------
         inputport_d:in std_logic_vector(31 downto 0);
-        inputport_q:out std_logic_vector(31 downto 0);
+        inputport_q:out std_logic_vector(31 downto 0); ----------
         readdata1_d:in std_logic_vector(31 downto 0);
-        readdata1_q:out std_logic_vector(31 downto 0);
+        readdata1_q:out std_logic_vector(31 downto 0);  ----------
         readdata2_d:in std_logic_vector(31 downto 0);
-        readdata2_q:out std_logic_vector(31 downto 0);
+        readdata2_q:out std_logic_vector(31 downto 0); --------
         writeaddress1_d: in std_logic_vector(2 downto 0);
-        writeaddress1_q:out std_logic_vector(2 downto 0);
+        writeaddress1_q:out std_logic_vector(2 downto 0);  ----------
         writeaddress2_d: in std_logic_vector(2 downto 0);
-        writeaddress2_q:out std_logic_vector(2 downto 0);
-        memtoreg_q: out std_logic;
-        memtoreg_d:in std_logic;
-        outputenable_q: out std_logic;
+        writeaddress2_q:out std_logic_vector(2 downto 0);  ----------
+        memtoreg_q: out std_logic; ---------
+        memtoreg_d:in std_logic; 
+        outputenable_q: out std_logic; ---------
         outputenable_d:in std_logic;
-        regwrite1_q: out std_logic;
+        regwrite1_q: out std_logic;  -------
         regwrite1_d:in std_logic;
-        regwrite2_q: out std_logic;
+        regwrite2_q: out std_logic; -------
         regwrite2_d:in std_logic;
-        inputenable_q:out std_logic;
-        inputenable_d:in std_logic;
-        memwrite_q:out std_logic;
-        memwrite_d:in std_logic
+        inputenable_q:out std_logic; ------
+        inputenable_d:in std_logic
+        -- memwrite_q:out std_logic; -------
+        -- memwrite_d:in std_logic
         );
 end mem_wb_reg;
 architecture dff of mem_wb_reg is
@@ -48,7 +48,7 @@ begin
         regwrite1_q<='0';
         regwrite2_q<='0';
         inputenable_q<='0';
-        memwrite_q<='0';
+        -- memwrite_q<='0';
     elsif clk'event and clk = '1' and enable = '1' then 
         inputport_q<=inputport_d;
         memory_q<=memory_d;
@@ -62,7 +62,7 @@ begin
         regwrite1_q<=regwrite1_d;
         regwrite2_q<=regwrite2_d;
         inputenable_q<=inputenable_d;
-        memwrite_q<=memwrite_d;
+        -- memwrite_q<=memwrite_d;
     end if;
 end process;
 end dff; 
